83365c1af2b3756b4357e054d0dc69a4b924eca0
Fernando Bergamini <fernandobr.89@hotmail.com>
2022-09-13 19:22:53 -0300

    primeiro commit

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,34 +1,31 @@
-context('Funcionalidade login', () => {
+/// <reference types="cypress" />
 
-  beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+context('Funcionalidade login', () => {
+  it('Deve fazer login com sucesso', () => {
+     cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+     cy.get('#username').type('aluno_ebac@teste.com')
+     cy.get('#password').type('teste@teste.com')
+     cy.get('.woocommerce-form > .button').click()
+     cy.get('.page-title').should('contain' , 'Minha conta')
+
     
   });
 
-  it('deve fazer login com sucesso', () => {
-   
-    cy.get('#username').type('aluno_ebac@teste.com')
-    cy.get('#password').type('teste@teste.com')
-    cy.get('.woocommerce-form >.button').click()
+  it('Deve exibir msg de erro ao inserir usuario invalidos', () => {
+    cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+     cy.get('#username').type('aluno_eba@teste.com')
+     cy.get('#password').type('teste@teste.com')
+     cy.get('.woocommerce-form > .button').click()
+     cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido')
+      
+  });
 
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, aluno')
-  })
-
-  it('deve exibir uma msg de erro ao inserir usuario invalido', () => {
-    
-    cy.get('#username').type('aluno_eba@teste.com')
-    cy.get('#password').type('teste@teste.com')
-    cy.get('.woocommerce-form >.button').click()
-
-    cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
-
-  })
-  it('deve exibir uma msg de erro ao inserir senha invalido', () => {
-   
-    cy.get('#username').type('aluno_ebac@teste.com')
-    cy.get('#password').type('teste@teste')
-    cy.get('.woocommerce-form >.button').click()
-
-    cy.get('.woocommerce-error').should('contain', 'A senha fornecida para o e-mail')
-  })
-})
+  it.only('Deve exibir msg de erro ao inserir senha invalidos', () => {
+    cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+     cy.get('#username').type('aluno_ebac@teste.com')
+     cy.get('#password').type('teste@teste')
+     cy.get('.woocommerce-form > .button').click()
+     cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail')
+      
+  });
+});
